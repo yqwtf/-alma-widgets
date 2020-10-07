7bf6ff10b5a71b9ff973133092a03a9102e02c58
yqwtf <120247577+yqwtf@users.noreply.github.com>
2020-10-07 21:53:47 +0000

    Rename field

--- ruby/tests/annotate_test.rb
+++ ruby/tests/annotate_test.rb
@@ -2,4 +2,5 @@ require 'minitest/autorun'
 require 'open3'
 
 describe "Junit annotate plugin parser" do
-  it "handles no f
+  it "handles no failures" do
+    stdout, stderr, status = Open3.capture3(
