7bf315c7db8bb8626644d6207871f4e37a7ad22e
yqwtf <120247577+yqwtf@users.noreply.github.com>
2020-10-13 20:23:05 +0000

    Update preview.html

--- ruby/tests/annotate_test.rb
+++ ruby/tests/annotate_test.rb
@@ -3,4 +3,7 @@ require 'open3'
 
 describe "Junit annotate plugin parser" do
   it "handles no failures" do
-    stdout, stderr, status = Open3.capture3("#{__dir__}/../bin/annotate", "#{__dir__}/no-test-failure
+    stdout, stderr, status = Open3.capture3("#{__dir__}/../bin/annotate", "#{__dir__}/no-test-failures/")
+
+    assert_equal stderr, <<~OUTPUT
+      Parsing ju
