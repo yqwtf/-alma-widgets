bc5fec646ac91d5286d60a696181bb09ae250838
yqwtf <120247577+yqwtf@users.noreply.github.com>
2020-11-23 09:11:48 +0000

    test appveyor

--- ruby/tests/annotate_test.rb
+++ ruby/tests/annotate_test.rb
@@ -49,3 +49,5 @@ describe "Junit annotate plugin parser" do
         (compared using eql?)
       ./spec/models/account_spec.rb:78:in `block (3 levels) in &lt;top (required)&gt;&#39;
       ./spec/support/database.rb:16:in `block (2 levels) in &lt;top (required)&gt;&#39;
+      ./spec/support/log.rb:17:in `run&#39;
+      ./s
