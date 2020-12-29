9263117aa4bf06b7527d921541c8314a4463ce5d
yqwtf <120247577+yqwtf@users.noreply.github.com>
2020-12-29 08:54:01 +0000

    entry point

--- ruby/tests/annotate_test.rb
+++ ruby/tests/annotate_test.rb
@@ -66,4 +66,4 @@ describe "Junit annotate plugin parser" do
              got: 500
       
         (compared using eql?)
-      ./spec/models/account_spec.rb:78:in `blo
+      ./spec/models/account_spec.rb:78:in `block (3 levels) in &lt;top (required)&gt;&
