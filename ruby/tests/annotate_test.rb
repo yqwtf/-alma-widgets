212749f054e32aace3e02bd74ff9e17e449e8b42
yqwtf <120247577+yqwtf@users.noreply.github.com>
2021-01-02 16:26:07 +0000

    Removing old code

--- ruby/tests/annotate_test.rb
+++ ruby/tests/annotate_test.rb
@@ -69,4 +69,8 @@ describe "Junit annotate plugin parser" do
       ./spec/models/account_spec.rb:78:in `block (3 levels) in &lt;top (required)&gt;&#39;
       ./spec/support/database.rb:16:in `block (2 levels) in &lt;top (required)&gt;&#39;
       ./spec/support/log.rb:17:in `run&#39;
-      ./spec/support/log.rb:66:in `block (2 levels) in &lt;top (required)&gt;&#39;</code
+      ./spec/support/log.rb:66:in `block (2 levels) in &lt;top (required)&gt;&#39;</code></pre>
+      
+      in <a href="#2">Job #2</a>
+      </details>
+
