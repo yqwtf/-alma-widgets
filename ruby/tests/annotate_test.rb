8faac843bb164548987d32acbe6fa94f71f0db39
yqwtf <120247577+yqwtf@users.noreply.github.com>
2021-01-04 14:07:53 +0000

    Add new commands

--- ruby/tests/annotate_test.rb
+++ ruby/tests/annotate_test.rb
@@ -75,4 +75,6 @@ describe "Junit annotate plugin parser" do
       </details>
 
       <details>
-      <summary><code>Account#maximum_jobs_added_by_pipeline_changer returns 700 if the account is XYZ 
+      <summary><code>Account#maximum_jobs_added_by_pipeline_changer returns 700 if the account is XYZ in spec.models.account_spec</code></summary>
+      
+     
