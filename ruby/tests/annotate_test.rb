d4124b4a4779e97f3f5fca42faaf45e72880cec1
yqwtf <120247577+yqwtf@users.noreply.github.com>
2021-01-06 10:13:35 +0000

    Oops again

--- ruby/tests/annotate_test.rb
+++ ruby/tests/annotate_test.rb
@@ -79,4 +79,5 @@ describe "Junit annotate plugin parser" do
       
       <p>expected: 700 got: 500 (compared using eql?)</p>
 
-      <pre><code>Failure/Error: expect(account.ma
+      <pre><code>Failure/Error: expect(account.maximum_jobs_added_by_pipeline_changer).to eql(250)
+      
