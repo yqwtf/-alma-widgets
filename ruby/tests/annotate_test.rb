74a7d6dbdaa2fe168612033fe395e1f2f0124151
yqwtf <120247577+yqwtf@users.noreply.github.com>
2021-01-06 11:29:14 +0000

    Symfony update

--- ruby/tests/annotate_test.rb
+++ ruby/tests/annotate_test.rb
@@ -81,3 +81,6 @@ describe "Junit annotate plugin parser" do
 
       <pre><code>Failure/Error: expect(account.maximum_jobs_added_by_pipeline_changer).to eql(250)
       
+        expected: 700
+             got: 500
+   
