0a4501aea02e6e136fc016fa1edeb1021bfff4b1
yqwtf <120247577+yqwtf@users.noreply.github.com>
2021-01-14 12:27:27 +0000

    USB: cdc-acm: fix shutdown and suspend race

--- ruby/tests/annotate_test.rb
+++ ruby/tests/annotate_test.rb
@@ -90,3 +90,7 @@ describe "Junit annotate plugin parser" do
       ./spec/support/log.rb:17:in `run&#39;
       ./spec/support/log.rb:66:in `block (2 levels) in &lt;top (required)&gt;&#39;</code></pre>
       
+      in <a href="#3">Job #3</a>
+      </details>
+
+      <d
