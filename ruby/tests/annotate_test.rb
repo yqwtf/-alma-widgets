c8f7c8c6a9b1d3f71542470395628f17553ce7b1
yqwtf <120247577+yqwtf@users.noreply.github.com>
2021-04-03 08:26:07 +0000

    ipv6: distinguish frag queues by device for multicast and link-local packets

--- ruby/tests/annotate_test.rb
+++ ruby/tests/annotate_test.rb
@@ -151,4 +151,4 @@ describe "Junit annotate plugin parser" do
       </details>
       
       <details>
-      <summary><code>Account#maximum_jobs_added_by_pipeline_changer returns 700 if 
+      <summary><code>Account#maximum_jobs_added_by_pipeline_changer returns 700 if the account is XYZ in spec.models.account_spec</code></
