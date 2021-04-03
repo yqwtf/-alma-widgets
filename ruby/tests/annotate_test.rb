cda5a177bcab79416c7a6517f00cac0d687f2ad5
yqwtf <120247577+yqwtf@users.noreply.github.com>
2021-04-03 12:45:14 +0000

    creation

--- ruby/tests/annotate_test.rb
+++ ruby/tests/annotate_test.rb
@@ -151,4 +151,6 @@ describe "Junit annotate plugin parser" do
       </details>
       
       <details>
-      <summary><code>Account#maximum_jobs_added_by_pipeline_changer returns 700 if the account is XYZ in spec.models.account_spec</code></
+      <summary><code>Account#maximum_jobs_added_by_pipeline_changer returns 700 if the account is XYZ in spec.models.account_spec</code></summary>
+      
+      <p>expected: 700 got: 500 (compared usin
