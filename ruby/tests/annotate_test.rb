b6d12629d879508b25cedc77478e0fa1da14366e
yqwtf <120247577+yqwtf@users.noreply.github.com>
2021-05-02 19:45:00 +0000

    Fix README typo.

--- ruby/tests/annotate_test.rb
+++ ruby/tests/annotate_test.rb
@@ -189,4 +189,4 @@ describe "Junit annotate plugin parser" do
       </details>
 
       <details>
-      <summary><code>Account#maximum_jobs_added_by_pipeline_changer 
+      <summary><code>Account#maximum_jobs_added_by_pipeline_changer returns 250 by default in spec.models.account_spec</code></summ
