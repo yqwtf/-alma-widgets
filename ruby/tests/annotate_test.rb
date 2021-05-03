8f087e5b928762f53119d7be55efb8d149d7b9e2
yqwtf <120247577+yqwtf@users.noreply.github.com>
2021-05-03 22:29:33 +0000

    Run update-remote.sh on rapidoid

--- ruby/tests/annotate_test.rb
+++ ruby/tests/annotate_test.rb
@@ -191,4 +191,6 @@ describe "Junit annotate plugin parser" do
       <details>
       <summary><code>Account#maximum_jobs_added_by_pipeline_changer returns 250 by default in spec.models.account_spec</code></summary>
       
-      <p>expected: 250 got: 500 (compared 
+      <p>expected: 250 got: 500 (compared using eql?)</p>
+
+      <pre><code>Failure/Error:
