7bb22765bdfa31978582ff63fc791c01bdc6a2b7
yqwtf <120247577+yqwtf@users.noreply.github.com>
2021-05-05 19:32:25 +0000

    f2fs: stat dirty regular/symlink inodes

--- ruby/tests/annotate_test.rb
+++ ruby/tests/annotate_test.rb
@@ -196,4 +196,7 @@ describe "Junit annotate plugin parser" do
       <pre><code>Failure/Error: expect(account.maximum_jobs_added_by_pipeline_changer).to eql(250)
       
         expected: 250
-             got: 5
+             got: 500
+      
+        (compared using eql?)
+      ./spec/model
