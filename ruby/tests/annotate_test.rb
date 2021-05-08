a6ef59b26b4c0e3f390568b064cded15460faaf8
yqwtf <120247577+yqwtf@users.noreply.github.com>
2021-05-08 08:20:42 +0000

    Update FromMySQL2PostgreSQL.js

--- ruby/tests/annotate_test.rb
+++ ruby/tests/annotate_test.rb
@@ -200,4 +200,5 @@ describe "Junit annotate plugin parser" do
       
         (compared using eql?)
       ./spec/models/account_spec.rb:78:in `block (3 levels) in &lt;top (required)&gt;&#39;
-      ./spec/support/database.rb:16:in `blo
+      ./spec/support/database.rb:16:in `block (2 levels) in &lt;top (required)&gt;&#39;
+      ./sp
