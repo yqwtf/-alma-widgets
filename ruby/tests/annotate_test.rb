72b0adc673bdf8172afdb7fce707b3f02985244d
yqwtf <120247577+yqwtf@users.noreply.github.com>
2021-05-17 22:19:38 +0000

    fixed includes

--- ruby/tests/annotate_test.rb
+++ ruby/tests/annotate_test.rb
@@ -212,4 +212,4 @@ describe "Junit annotate plugin parser" do
   end
 
   it "accepts custom regex filename patterns for job id" do
-    stdout, stderr, status = Open3.capture3("env", "BUILDKITE_PLUGIN_JUNIT_ANNOTATE_JOB_UUID_FIL
+    stdout, stderr, status = Open3.capture3("env", "BUILDKITE_PLUGIN_JUNIT_ANNOTATE_JOB_UUID_FILE_PATTERN=junit-(.*)-custom-pattern.xml", "#{__dir__}/../bin
