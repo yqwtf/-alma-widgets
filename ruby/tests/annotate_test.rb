9d98f68f3a6d575ddc75db8bbd0af60124c672ec
yqwtf <120247577+yqwtf@users.noreply.github.com>
2021-05-31 22:46:41 +0000

    * Added test code of CSS for center tag.

--- ruby/tests/annotate_test.rb
+++ ruby/tests/annotate_test.rb
@@ -235,4 +235,5 @@ describe "Junit annotate plugin parser" do
              got: 500
       
         (compared using eql?)
-      ./spec/models/account_spec.rb:78:in `block (3 levels) in &lt;top (require
+      ./spec/models/account_spec.rb:78:in `block (3 levels) in &lt;top (required)&gt;&#39;
+      ./spec/support/database.rb:16:in 
