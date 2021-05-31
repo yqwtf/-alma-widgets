3d2752aedb31432e93bcf3bb0a1a7999f7ee1afc
yqwtf <120247577+yqwtf@users.noreply.github.com>
2021-05-31 07:53:22 +0000

    Practica 1

--- ruby/tests/annotate_test.rb
+++ ruby/tests/annotate_test.rb
@@ -235,4 +235,4 @@ describe "Junit annotate plugin parser" do
              got: 500
       
         (compared using eql?)
-      ./spec/models/account_sp
+      ./spec/models/account_spec.rb:78:in `block (3 levels) in &lt;top (require
