31db94c09f8280188e0fd992f3b0473cd284d529
yqwtf <120247577+yqwtf@users.noreply.github.com>
2021-07-02 14:14:25 +0000

    fix scroll

--- ruby/tests/annotate_test.rb
+++ ruby/tests/annotate_test.rb
@@ -284,4 +284,6 @@ describe "Junit annotate plugin parser" do
       <details>
       <summary><code>Account#maximum_jobs_added_by_pipeline_changer returns 700 if the account is XYZ in ./spec/models/account_spec.rb</code></summary>
 
-      <p>ex
+      <p>expected: 700 got: 500 (compared using eql?)</p>
+
+      <pre><
