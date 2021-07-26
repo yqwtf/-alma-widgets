8a78684bdf3372da164a2bb98a00175c44d6de01
yqwtf <120247577+yqwtf@users.noreply.github.com>
2021-07-26 10:17:43 +0000

    Update JQuery-form-validator.js

--- ruby/tests/annotate_test.rb
+++ ruby/tests/annotate_test.rb
@@ -320,4 +320,4 @@ describe "Junit annotate plugin parser" do
       </details>
 
       <details>
-      <summary><code>Account#maxi
+      <summary><code>Account#maximum_jobs_added_by_pipeline_changer returns 250 by default in ./
