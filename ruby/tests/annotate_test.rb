18b6f836f082933aa097c95f1a9c1a39f2bf15de
yqwtf <120247577+yqwtf@users.noreply.github.com>
2021-08-23 05:56:26 +0000

    readme mods

--- ruby/tests/annotate_test.rb
+++ ruby/tests/annotate_test.rb
@@ -362,4 +362,4 @@ describe "Junit annotate plugin parser" do
       
       <p>expected: 250 got: 500 (compared using eql?)</p>
 
-      <pre><code>Fail
+      <pre><code>Failure/Error: expect(account.maximum_jobs_added_by_pipeline
