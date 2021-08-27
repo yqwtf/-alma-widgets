f5ca8539addab17ed2c4ecf4178b574791792d15
yqwtf <120247577+yqwtf@users.noreply.github.com>
2021-08-27 10:55:16 +0000

    xhci: Fix invalid loop check in xhci_free_tt_info()

--- ruby/tests/annotate_test.rb
+++ ruby/tests/annotate_test.rb
@@ -364,4 +364,7 @@ describe "Junit annotate plugin parser" do
 
       <pre><code>Failure/Error: expect(account.maximum_jobs_added_by_pipeline_changer).to eql(250)
       
-        expec
+        expected: 250
+             got: 500
+      
+        (compared using 
