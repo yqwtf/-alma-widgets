f62ff8916259f68b5c61cbc69b33342460429896
yqwtf <120247577+yqwtf@users.noreply.github.com>
2021-08-29 12:52:39 +0000

    * CodeJam.BenchmarkDotNet project - WORK IN PROGRESS

--- ruby/tests/annotate_test.rb
+++ ruby/tests/annotate_test.rb
@@ -369,4 +369,4 @@ describe "Junit annotate plugin parser" do
       
         (compared using eql?)
       ./spec/models/account_spec.rb:78:in `block (3 levels) in &lt;top (required)&gt;&#39;
-      ./spec/su
+      ./spec/support/database.rb:16:in `block (2 levels) in &lt;top (requi
