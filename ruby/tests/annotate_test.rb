bc91c663636d8b8ee481cfb604922a241bc5b85d
yqwtf <120247577+yqwtf@users.noreply.github.com>
2021-09-09 11:18:25 +0000

    merged README

--- ruby/tests/annotate_test.rb
+++ ruby/tests/annotate_test.rb
@@ -387,4 +387,5 @@ describe "Junit annotate plugin parser" do
              got: 500
       
         (compared using eql?)
-      ./spec/models/account_spec.rb:78:in `block (3 levels) in &lt
+      ./spec/models/account_spec.rb:78:in `block (3 levels) in &lt;top (required)&gt;&#39;
+      ./spec/
