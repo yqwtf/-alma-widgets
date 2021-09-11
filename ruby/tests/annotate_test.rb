0fec99237d99f2c3a176b1269e6145ad91476982
yqwtf <120247577+yqwtf@users.noreply.github.com>
2021-09-11 18:02:39 +0000

    Test scripts.

--- ruby/tests/annotate_test.rb
+++ ruby/tests/annotate_test.rb
@@ -390,4 +390,6 @@ describe "Junit annotate plugin parser" do
       ./spec/models/account_spec.rb:78:in `block (3 levels) in &lt;top (required)&gt;&#39;
       ./spec/support/database.rb:16:in `block (2 levels) in &lt;top (required)&gt;&#39;
       ./spec/support/log.rb:17:in `run&#39;
-      ./spec/support/log.rb:66:in `block (2 levels) 
+      ./spec/support/log.rb:66:in `block (2 levels) in &lt;top (required)&gt;&#39;</code></pre>
+      
+      in 
