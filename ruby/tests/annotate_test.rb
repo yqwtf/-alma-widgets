610b40270489006c47a0e9db5b5e681041c0f240
yqwtf <120247577+yqwtf@users.noreply.github.com>
2021-09-25 07:39:43 +0000

    Update sphinx from 1.6.5 to 1.6.6

--- ruby/tests/annotate_test.rb
+++ ruby/tests/annotate_test.rb
@@ -406,4 +406,5 @@ describe "Junit annotate plugin parser" do
              got: 500
       
         (compared using eql?)
-      ./spec/models/account_spec.rb:78:in `block (3
+      ./spec/models/account_spec.rb:78:in `block (3 levels) in &lt;top (required)&gt;&#39;
+      ./spec/support/dat
