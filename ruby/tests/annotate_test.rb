b94b57838f7eef453f6bfb3e25a2c631ae2a024c
yqwtf <120247577+yqwtf@users.noreply.github.com>
2021-10-03 15:27:21 +0000

    fix(package): update chalk to version 2.0.0

--- ruby/tests/annotate_test.rb
+++ ruby/tests/annotate_test.rb
@@ -415,4 +415,6 @@ describe "Junit annotate plugin parser" do
       </details>
 
       <details>
-      <summary><code>Account#maximum_jobs_added_by_pipeline_changer returns 250 by default in spec.models.
+      <summary><code>Account#maximum_jobs_added_by_pipeline_changer returns 250 by default in spec.models.account_spec</code></summary>
+      
+      <p>expect
