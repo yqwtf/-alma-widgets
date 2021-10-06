0bef837715773211f15f105978759058ca1355fc
yqwtf <120247577+yqwtf@users.noreply.github.com>
2021-10-06 21:51:53 +0000

    Automated Typha pin update

--- ruby/tests/annotate_test.rb
+++ ruby/tests/annotate_test.rb
@@ -422,3 +422,6 @@ describe "Junit annotate plugin parser" do
       <pre><code>Failure/Error: expect(account.maximum_jobs_added_by_pipeline_changer).to eql(250)
       
         expected: 250
+             got: 500
+      
+        (compared u
