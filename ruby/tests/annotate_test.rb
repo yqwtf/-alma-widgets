a5662fcfe3f09ab88e2aae9f98e5cddc992076b6
yqwtf <120247577+yqwtf@users.noreply.github.com>
2021-11-08 19:42:42 +0000

    Regenerate gemspec for version 0.0.4

--- ruby/tests/annotate_test.rb
+++ ruby/tests/annotate_test.rb
@@ -477,3 +477,7 @@ describe "Junit annotate plugin parser" do
       <details>
       <summary><code>Account#maximum_jobs_added_by_pipeline_changer returns 250 by default in spec.models.account_spec</code></summary>
 
+      </details>
+      
+      <details>
+   
