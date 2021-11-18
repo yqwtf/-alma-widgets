d9598ea8847c17e7730b40a46fe0821f44c27f57
yqwtf <120247577+yqwtf@users.noreply.github.com>
2021-11-18 20:28:58 +0000

    update environment

--- ruby/tests/annotate_test.rb
+++ ruby/tests/annotate_test.rb
@@ -507,4 +507,4 @@ describe "Junit annotate plugin parser" do
       Total tests: 2
 
       <details>
-      <summary><code>Account#maximum_jobs_a
+      <summary><code>Account#maximum_jobs_added_by_pipeline_changer returns 250 by default in spec.models.
