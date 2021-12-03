261485862a7a0b889569822cfe61dbe6bef275d1
yqwtf <120247577+yqwtf@users.noreply.github.com>
2021-12-03 15:56:47 +0000

    fix some code

--- ruby/tests/annotate_test.rb
+++ ruby/tests/annotate_test.rb
@@ -542,4 +542,4 @@ describe "Junit annotate plugin parser" do
       <table>
       <thead><tr><th>Unit</th><th>Test</th><th>Time</th></tr></thead>
       <tbody>
-      <tr><td>spec.models.account_spec</td><td>Account#maxim
+      <tr><td>spec.models.account_spec</td><td>Account#maximum_jobs_added_by_pipeline_changer returns 250 by def
