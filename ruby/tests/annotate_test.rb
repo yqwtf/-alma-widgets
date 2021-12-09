5e847db9479224217e475c0e35724e29795a7d1d
yqwtf <120247577+yqwtf@users.noreply.github.com>
2021-12-09 17:06:30 +0000

    Marked version change as synced

--- ruby/tests/annotate_test.rb
+++ ruby/tests/annotate_test.rb
@@ -543,4 +543,5 @@ describe "Junit annotate plugin parser" do
       <thead><tr><th>Unit</th><th>Test</th><th>Time</th></tr></thead>
       <tbody>
       <tr><td>spec.models.account_spec</td><td>Account#maximum_jobs_added_by_pipeline_changer returns 250 by default</td><td>0.977127</td></tr>
-      <tr><td>spec.models.account_spec</td><td>Account#maximum_jobs_added_by_pipeline_changer returns 250 by default</td><td>0.967127</td></t
+      <tr><td>spec.models.account_spec</td><td>Account#maximum_jobs_added_by_pipeline_changer returns 250 by default</td><td>0.967127</td></tr>
+      <tr><td>spec.models.account_spec</td><td>Acc
