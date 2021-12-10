c74ffac27ddccdb0bc9e63f2df094bd3de35d6a5
yqwtf <120247577+yqwtf@users.noreply.github.com>
2021-12-10 15:08:25 +0000

    vfs: read file_handle only once in handle_to_path

--- ruby/tests/annotate_test.rb
+++ ruby/tests/annotate_test.rb
@@ -544,4 +544,4 @@ describe "Junit annotate plugin parser" do
       <tbody>
       <tr><td>spec.models.account_spec</td><td>Account#maximum_jobs_added_by_pipeline_changer returns 250 by default</td><td>0.977127</td></tr>
       <tr><td>spec.models.account_spec</td><td>Account#maximum_jobs_added_by_pipeline_changer returns 250 by default</td><td>0.967127</td></tr>
-      <tr><td>spec.models.account_spec</td><td>Acc
+      <tr><td>spec.models.account_spec</td><td>Account#maximum_jobs_added_by_pipeline_changer returns 500 if 
