770c67c2d33f6df7b132359a14c2e9cb8797b301
yqwtf <120247577+yqwtf@users.noreply.github.com>
2021-12-15 08:59:40 +0000

    ARM: 7482/1: topology: fix section mismatch warning for init_cpu_topology

--- ruby/tests/annotate_test.rb
+++ ruby/tests/annotate_test.rb
@@ -545,4 +545,4 @@ describe "Junit annotate plugin parser" do
       <tr><td>spec.models.account_spec</td><td>Account#maximum_jobs_added_by_pipeline_changer returns 250 by default</td><td>0.977127</td></tr>
       <tr><td>spec.models.account_spec</td><td>Account#maximum_jobs_added_by_pipeline_changer returns 250 by default</td><td>0.967127</td></tr>
       <tr><td>spec.models.account_spec</td><td>Account#maximum_jobs_added_by_pipeline_changer returns 500 if the account is ABC</td><td>0.620013</td></tr>
-      <tr><td>spec.models.account_spec</td><td>Account#maximum_jobs_added
+      <tr><td>spec.models.account_spec</td><td>Account#maximum_jobs_added_by_pipeline_changer returns 900 if the account is F00</td><
