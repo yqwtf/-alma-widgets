5aaaec862701841ef1001bfbb045c4e971708bf1
yqwtf <120247577+yqwtf@users.noreply.github.com>
2021-12-20 16:01:01 +0000

    Add link to README

--- ruby/tests/annotate_test.rb
+++ ruby/tests/annotate_test.rb
@@ -546,4 +546,4 @@ describe "Junit annotate plugin parser" do
       <tr><td>spec.models.account_spec</td><td>Account#maximum_jobs_added_by_pipeline_changer returns 250 by default</td><td>0.967127</td></tr>
       <tr><td>spec.models.account_spec</td><td>Account#maximum_jobs_added_by_pipeline_changer returns 500 if the account is ABC</td><td>0.620013</td></tr>
       <tr><td>spec.models.account_spec</td><td>Account#maximum_jobs_added_by_pipeline_changer returns 900 if the account is F00</td><td>0.520013</td></tr>
-      <tr><td>spec.models.account_spec</td><td>Account#maximum_jobs_added_by_pipeline_change
+      <tr><td>spec.models.account_spec</td><td>Account#maximum_jobs_added_by_pipeline_changer returns 700 if the account is XYZ</td><td>0.420013</td></t
